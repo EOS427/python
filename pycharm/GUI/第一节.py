f355348c0797eaca4be8257a9ba8f321beb81492
EOS427 <eos1003202427@gmail.com>
2026-08-29 00:43:26 +0800

    feat(GUI): 给第一页示例添加点击切换文本的按钮和逻辑
    新增了StringVar绑定标签文本，添加点击按钮切换标签显示内容的功能

--- pycharm/GUI/第一节.py
+++ pycharm/GUI/第一节.py
@@ -4,13 +4,35 @@ window=tk.Tk()
 window.title("my_window")
 window.geometry("200x100")
 
+var=tk.StringVar()
+
 label=tk.Label(
-    window,text="Label",
+    window,
+    textvariable=var,
     bg="green",
     font=("Arial",12),
     width=15,
     height=2
     )
 label.pack()
+
+on_hit=False
+def hit_me():
+    global on_hit
+    if on_hit==False:
+        on_hit=True
+        var.set("you hit me")
+    else:
+        on_hit=False
+        var.set("")
+
+button=tk.Button(
+    window,
+    text="hit me",
+    width=15,
+    height=2,
+    command=hit_me
+    )
+button.pack()
 
 window.mainloop()
