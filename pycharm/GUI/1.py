4efbd567c8e6974efef4c6f25ba90ed167f7a2c3
EOS427 <eos1003202427@gmail.com>
2026-08-29 23:36:56 +0800

    1.自动居中=第一版

--- pycharm/GUI/1.py
+++ pycharm/GUI/1.py
@@ -1,7 +1,24 @@
 import tkinter as tk
 
 window=tk.Tk()
-
 window.title("new window")
+win_size=["300","300"]
+# window.geometry('x'.join(win_size))
+window.update_idletasks()
 
+scr_half_height=window.maxsize()[1]//2
+scr_half_width=window.maxsize()[0]//2
+
+# win_half_height=window.winfo_height()//2
+# win_half_width=window.winfo_width()//2
+
+win_half_height=int(win_size[0])//2
+win_half_width=int(win_size[1])//2
+
+x_coor=scr_half_width-win_half_width
+y_coor=scr_half_height-win_half_height
+
+window.geometry('x'.join(win_size)+f"+{x_coor}+{y_coor}")
+
+window.geometry()
 window.mainloop()
