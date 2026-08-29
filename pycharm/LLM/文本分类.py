04f19629807998437ace6fc9b6da388dfb3b3e25
EOS427 <eos1003202427@gmail.com>
2026-08-29 22:39:15 +0800

    1初始代码

--- pycharm/LLM/文本分类.py
+++ pycharm/LLM/文本分类.py
@@ -37,3 +37,22 @@ def evaluate_performance(y_true, y_pred):
     print(performance)
 
 evaluate_performance(data["test"]["label"], y_pred)
+
+
+pipe = pipeline(
+"text2text-generation",
+model="google/flan-t5-small",
+device="cuda:0"
+)
+
+prompt = "Is the following sentence positive or negative? "
+data = data.map(lambda example: {"t5": prompt + example['text']})
+data
+
+y_pred = []
+for output in tqdm(pipe(KeyDataset(data["test"], "t5")),
+total=len(data["test"])):
+    text = output[0]["generated_text"]
+    y_pred.append(0 if text == "negative" else 1)
+
+evaluate_performance(data["test"]["label"], y_pred)
